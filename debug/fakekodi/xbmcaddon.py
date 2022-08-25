90d77689fe4e8f886e756c7d1d2679620b0e3f93
wastis <wasti_s@t-online.de>
2022-08-25 12:41:39 +0200

    v2.1.5

--- debug/fakekodi/xbmcaddon.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Addon():
-	def __init__(self):
-		pass
-
-	def getAddonInfo(self,info):
-		if info == 'path':
-			return os.getcwd()
--- debug/fakekodi/xbmcvfs.py
+++
@@ -1,2 +0,0 @@
-def translatePath(path):
-	return path
